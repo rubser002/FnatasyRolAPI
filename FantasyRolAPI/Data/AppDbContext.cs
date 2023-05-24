232641dc8623f6fdf0f6f939b746dddaa0b04ef4
Ruben Serrano Pardos <rserrano@efor.es>
2023-05-24 16:52:43 +0200

    Init

---
+++ FantasyRolAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FantasyRolAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FantasyRolAPI.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ FantasyRolAPI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FantasyRolAPI.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmailConfirmed { get; set; }
+
+    }
+}
